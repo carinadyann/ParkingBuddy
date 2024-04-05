04670482e0226a611a9de118a3f113d69be02b47
Carina Adrianzen <70672577+carinadyann@users.noreply.github.com>
2024-04-04 23:33:48 -0700

    Begin Top Nav

---
+++ source_code/navigation/StackNav.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { View, Button, Text, Animated } from "react-native";
+import { NavigatioonContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+function Settings({ navigation }) {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#8BD7D2",
+            }}
+        >
+            <Text style={{ fontSize: 20, color: '#00BD9D', fontWeight: "800" }}>
+                Settings is here!
+            </Text>
+        </View>
+    );
+}
