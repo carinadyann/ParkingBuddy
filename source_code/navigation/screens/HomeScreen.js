0441b094ff938c549a534e785494305287a4ea76
Carina Adrianzen <70672577+carinadyann@users.noreply.github.com>
2024-04-15 23:10:08 -0700

    countdown clock implementation begin

--- source_code/navigation/screens/HomeScreen.js
+++ source_code/navigation/screens/HomeScreen.js
@@ -2,13 +2,21 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import * as Font from 'expo-font';
 import BoxContainer from '../BoxContainer';
+import DisplayComponent from '../DisplayComponent';
 //import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 
 export default function HomeScreen({ navigation }) {
+    const [time, setTime] = React.useState({ms: 0, s: 0, m: 0, h: 0});
     return (
         <View style={styles.container}>
             <BoxContainer style={styles.clock}>
-                <Text onPress={() => alert('This is the "Home" screen.')}>Home Screen</Text>
+                <BoxContainer style={styles.clockHolder}>
+                    <BoxContainer style={styles.stopwatch}>
+                        {/* clock held here */}
+                        {/* <Text onPress={() => alert('This is the "Home" screen.')}>Home Screen</Text> */}
+                        <DisplayComponent time={time}/>
+                    </BoxContainer>
+                </BoxContainer>
             </BoxContainer>
         </View>
     );
@@ -26,5 +34,22 @@ const styles = StyleSheet.create({
         margin: 10,
         color: "white",
         fontFamily: 'Arial Rounded MT Bold',
+        borderRadius: 25,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+    },
+    parking: {
+        backgroundColor: "#49C6E5",
+    },
+    clockHolder: {
+        width: 100,
+        background: "black",
+        margin: 'auto',
+        // position: 'relative',
+    }, 
+    stopwatch: {
+        padding: 60,
+        textAlign: 'center',
     },
 });
