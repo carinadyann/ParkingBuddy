7f886eeb711a17e9a9f199d5451b6c1b1851f707
Carina Adrianzen <70672577+carinadyann@users.noreply.github.com>
2024-04-16 17:25:53 -0700

    new container

--- source_code/navigation/screens/HomeScreen.js
+++ source_code/navigation/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import * as Font from 'expo-font';
 import BoxContainer from '../BoxContainer';
 import DisplayComponent from '../DisplayComponent';
+import {styles} from '../style';
 //import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 
 export default function HomeScreen({ navigation }) {
@@ -18,49 +19,12 @@ export default function HomeScreen({ navigation }) {
                     </BoxContainer>
                 </BoxContainer>
             </BoxContainer>
+            <BoxContainer style={styles.boxDark}>
+                <Text style={styles.text}>Zone: <Text>(Replace With Function)</Text></Text>
+                <Text style={styles.text}>Parking Spot: <Text>(Replace With Function)</Text></Text>
+                <Text style={styles.text}>Duration Type: <Text>(Replace With Function)</Text></Text>
+                <Button onPress={() => alert('This is the "Home" screen.')} title="Submit" style={styles.button}/>
+            </BoxContainer>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "white",
-        padding: 20,
-    },
-    boxLight: {
-        backgroundColor: "#54DEFD",
-        padding: 10,
-        margin: 10,
-        color: "white",
-        fontFamily: 'Arial Rounded MT Bold',
-        borderRadius: 25,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-    },
-    boxDark: {
-        backgroundColor: "#49C6E5",
-        padding: 10,
-        margin: 10,
-        color: "white",
-        fontFamily: 'Arial Rounded MT Bold',
-        borderRadius: 25,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-    },
-    parking: {
-        backgroundColor: "#49C6E5",
-    },
-    clockHolder: {
-        width: 100,
-        background: "black",
-        margin: 'auto',
-        // position: 'relative',
-    }, 
-    stopwatch: {
-        padding: 60,
-        textAlign: 'center',
-    },
-});
