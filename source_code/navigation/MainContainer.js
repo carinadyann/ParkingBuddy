0ae882c2bea76ee6f3dc790ed575b649e4e35f4e
Carina Adrianzen <cadrianzen31@cox.net>
2024-07-04 00:00:49 -0700

    login screen needs fixing

--- source_code/navigation/MainContainer.js
+++ source_code/navigation/MainContainer.js
@@ -1,73 +1,68 @@
-import * as React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-// Screens
 import HomeScreen from './screens/HomeScreen';
-import LocatorScreen from './screens/LocatorScreen';
+import LocatorScreen from './screens/LocatorScreen'
 import PaymentScreen from './screens/PaymentScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import SettingsScreen from './screens/SettingsScreen';
 
-// Screen names
+const Tab = createBottomTabNavigator();
+
 const homeName = "Home";
 const locatorName = "Locator";
 const paymentName = "Payment";
 const profileName = "Profile";
 const settingsName = "Settings";
 
-const Tab = createBottomTabNavigator();
+function MainContainer({ onLogout }) {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-function MainContainer() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator
-                initialRouteName={homeName}
-                screenOptions={({ route }) => ({
-                    headerShown: false,
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
-                        let rn = route.name;
+            if (route.name === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === locatorName) {
+              iconName = focused ? 'compass' : 'compass-outline';
+            } else if (route.name === paymentName) {
+              iconName = focused ? 'card' : 'card-outline';
+            } else if (route.name === profileName) {
+              iconName = focused ? 'person-circle' : 'person-circle-outline';
+            } else if (route.name === settingsName) {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
 
-                        if (rn === homeName) {
-                            iconName = focused ? 'home' : 'home-outline';
-                        } else if (rn === locatorName) {
-                            iconName = focused ? 'compass' : 'compass-outline';
-                        } else if (rn === paymentName) {
-                            iconName = focused ? 'card' : 'card-outline';
-                        } else if (rn === profileName) {
-                            iconName = focused ? 'person-circle' : 'person-circle-outline';
-                        } else if (rn === settingsName) {
-                            iconName = focused ? 'settings' : 'settings-outline';
-                        }
-
-                        return <Ionicons name={iconName} size={size} color={color} />;
-                    },
-                    tabBarActiveTintColor: 'white',
-                    tabBarInactiveTintColor: '#00BD9D',
-                    tabBarLabelStyle: {
-                        paddingBottom: 3,
-                        fontSize: 10,
-                    },
-                    tabBarItemStyle: {
-                        backgroundColor: '#8BD7D2',
-                    },
-                    tabBarStyle: {
-                        padding: 0,
-                        height: 80,
-                    },
-                })}
-            >
-                {/* <Tab.Screen name={homeName} component={TopBarNavigator}/> */}
-                <Tab.Screen name={homeName} component={HomeScreen} />
-                <Tab.Screen name={locatorName} component={LocatorScreen} />
-                <Tab.Screen name={paymentName} component={PaymentScreen} />
-                <Tab.Screen name={profileName} component={ProfileScreen} />
-                <Tab.Screen name={settingsName} component={SettingsScreen} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: '#00BD9D',
+          tabBarLabelStyle: {
+            paddingBottom: 3,
+            fontSize: 10,
+          },
+          tabBarItemStyle: {
+            backgroundColor: '#8BD7D2',
+          },
+          tabBarStyle: {
+            padding: 0,
+            height: 80,
+          },
+        })}
+      >
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={locatorName} component={LocatorScreen} />
+        <Tab.Screen name={paymentName} component={PaymentScreen} />
+        <Tab.Screen name={profileName} component={ProfileScreen} />
+        <Tab.Screen name={settingsName} component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default MainContainer;
