655febbc5ecedcfe8ca2532c3d47ac133fd8c725
Carina Adrianzen <cadrianzen31@cox.net>
2024-07-03 23:39:10 -0700

    merge all

--- source_code/App.js
+++ source_code/App.js
@@ -1,46 +1,11 @@
 import * as React from 'react';
-import MainContainer from './navigation/MainContainer';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import StackNavigator from './navigation/StackNav'; // might need to change
-import TopTabNavigator from './navigation/TopBarNav'; // might need to change
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { AsyncStorage } from 'react-native';
-import LoginScreen from './navigation/screens/LoginScreen';
+import MainContainer from './navigation/MainContainer'; // Ensure this path is correct
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-
-  React.useEffect(() => {
-    const checkLoginStatus = async () => {
-      const token = await AsyncStorage.getItem('userToken');
-      if (token) {
-        setIsLoggedIn(true);
-      }
-    };
-
-    checkLoginStatus();
-  }, []);
-
-  const handleLoginSuccess = async() => {
-    await AsyncStorage.setItem('userToken', 'abc123');
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout = async () => {
-    await AsyncStorage.removeItem('userToken');
-    setIsLoggedIn(false);
-  };
-
-  return(
+  return (
     <SafeAreaProvider>
-    {isLoggedIn ? (
-      <View>
-        <MainContainer />
-        <Button title="Logout" onPress={handleLogout}/>
-      </View>
-    ) : (
-      <LoginScreen onLoginSuccess={handleLoginSuccess} />
-    )}
+      <MainContainer />
     </SafeAreaProvider>
   );
 }
